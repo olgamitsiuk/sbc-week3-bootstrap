9f153fdc8116b426180d452349d2afbe45798c5a
Mitsiuk Olha <mitsyuk.olga@gmail.com>
2024-10-08 11:13:55 +0100

    add jquery functionality

--- script.js
+++ script.js
@@ -45,38 +45,30 @@ $(document).ready(function() {
 
       // buy now button functionality
       $('.card-body button:contains("Buy now")').click(function(e) {
-      e.preventDefault();
-      var $button = $(this);
-      var productName = $button.closest('.card-body').find('.card-title').text();
-      var productPrice = $button.closest('.card-body').find('.fs-4').text();
+        e.preventDefault();
+        var $button = $(this);
+        var productName = $button.closest('.card-body').find('.card-title').text();
+        var productPrice = $button.closest('.card-body').find('.fs-4').text();
 
-      setTimeout(function() {
-        $('#quickCheckoutForm').submit(function(e) {
-                e.preventDefault();
-                var name = $('#customerName').val();
-                var email = $('#customerEmail').val();
+        setTimeout(function() {
+                $('#quickCheckoutForm').submit(function(e) {
+                    e.preventDefault();
+                    var name = $('#customerName').val();
+                    var email = $('#customerEmail').val();
 
-       $('#quickCheckoutForm').submit(function(e) {
-                e.preventDefault();
-                var name = $('#customerName').val();
-                var email = $('#customerEmail').val();
+                    $('.modal-body').html(`
+                        <h4>Thank you for your purchase, ${name}!</h4>
+                        <p>We've sent a confirmation email to ${email}.</p>
+                        <p>You will receive your order of "${productName}" soon.</p>
+                    `);
+                });
+            
+            $('#modalProductName').text(productName);
+            $('#modalProductPrice').text(productPrice);
 
-                $('.modal-body').html(`
-                    <h4>Thank you for your purchase, ${name}!</h4>
-                    <p>We've sent a confirmation email to ${email}.</p>
-                    <p>You will receive your order of "${productName}" soon.</p>
-                `);
-            });
-        })
-        $('#modalProductName').text(productName);
-        $('#modalProductPrice').text(productPrice);
-
-        // Show modal
-        var buyNowModal = new bootstrap.Modal(document.getElementById('buyNowModal'));
-        buyNowModal.show();
-
-        // Reset button
-        $button.removeClass('btn-success').html('Buy now');
-    }, 200);         
-      })
-  })
+            // Show modal
+            var buyNowModal = new bootstrap.Modal(document.getElementById('buyNowModal'));
+            buyNowModal.show();
+        }, 200);
+    });
+});
